5af200820ce9bde95242fc9b57a5b71781697a43
danielmlduarte <danielduarte.tryber@gmail.com>
2021-02-09 22:33:17 -0300

    Updates

---
+++ precache-manifest.3d81cfe5d0f8ac916f48b548811608b0.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464a3470a112cbe080b53a6f0bf0192c",
+    "url": "/app-recipes/index.html"
+  },
+  {
+    "revision": "25e947072d98fcd918bd",
+    "url": "/app-recipes/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "84e0eb4a354c43e2454a",
+    "url": "/app-recipes/static/css/main.3248b53e.chunk.css"
+  },
+  {
+    "revision": "25e947072d98fcd918bd",
+    "url": "/app-recipes/static/js/2.4b8181cf.chunk.js"
+  },
+  {
+    "revision": "9353b4df7938eafd5f8e3a0cf5a68c12",
+    "url": "/app-recipes/static/js/2.4b8181cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e0eb4a354c43e2454a",
+    "url": "/app-recipes/static/js/main.48cf574a.chunk.js"
+  },
+  {
+    "revision": "65201d6cfe4d168d3abd",
+    "url": "/app-recipes/static/js/runtime-main.30185218.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/app-recipes/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/app-recipes/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/app-recipes/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "88a6db57627d6b4f6b046f5f26a6f465",
+    "url": "/app-recipes/static/media/facebookIcon.88a6db57.png"
+  },
+  {
+    "revision": "dc610311496ff8edfb022a6ac6b74806",
+    "url": "/app-recipes/static/media/instagramIcon.dc610311.jpg"
+  },
+  {
+    "revision": "260d496fd401dddb28fc8f69a2b8fd62",
+    "url": "/app-recipes/static/media/logoDoAppEscrita.260d496f.jpg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/app-recipes/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/app-recipes/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/app-recipes/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/app-recipes/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "461089b8b31602c9c75ea2c022e55783",
+    "url": "/app-recipes/static/media/whatsAppIcon.461089b8.jpg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/app-recipes/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
